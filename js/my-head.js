e36083d95958bf6711e3227cb93fe6cb226eee9c
ak-office-pvt <129983417+ak-office-pvt@users.noreply.github.com>
2025-09-11 16:36:02 +0530

    Update my-head.js

--- js/my-head.js
+++ js/my-head.js
@@ -1,9 +1,10 @@
 function createHead() {
+    console.log("Loading head content..."); // Debug: Check if head is loading
     const headHTML = `
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="theme-color" content="#161616">
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/assets/img/favicon.ico" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/ajax/libs/bootstrap/4.3.1/css/bootstrap.min.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -13,13 +14,14 @@ function createHead() {
         <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700&display=swap" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/ajax/libs/bootstrap/4.3.1/js/bootstrap.min.js"></script>
         <script src="/js/click-heart.js"></script>
         <script src="/js/menu/list.js"></script>
     `;
     
     const headElement = document.head;
     headElement.innerHTML = headHTML;
+    console.log("Head content loaded successfully.");
 }
 
 document.addEventListener('DOMContentLoaded', createHead);
