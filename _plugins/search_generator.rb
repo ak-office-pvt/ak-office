f8971793a66a1103007d45e29f3256a3bd488eab
ak-office-pvt <129983417+ak-office-pvt@users.noreply.github.com>
2025-11-01 16:01:09 +0530

    Update search_generator.rb

--- _plugins/search_generator.rb
+++ _plugins/search_generator.rb
@@ -1,3 +1,5 @@
+require 'json'  # <-- YEH LINE ADD KARNA ZAROORI HAI!
+
 module Jekyll
   class SearchIndexGenerator < Generator
     safe true
@@ -6,20 +8,33 @@ module Jekyll
     def generate(site)
       json = []
       site.pages.each do |page|
-        next if page.path.start_with?('_') || page.path.end_with?('.xml')
+        # Skip layout files, XML, and hidden
+        next if page.path.start_with?('_')
+        next if page.path.end_with?('.xml')
         next if page.data['search'] == false
 
-        title = page.data['title'] || page.name.gsub(/[-_]/, ' ').gsub('.html', '').capitalize
-        body = page.content.strip[0..200] rescue ""
+        # Title: from front matter, else from filename
+        title = page.data['title']
+        title ||= page.name
+                  .gsub('.html', '')
+                  .gsub(/[-_]/, ' ')
+                  .split
+                  .map(&:capitalize)
+                  .join(' ')
+
+        # Body: first 200 chars of content
+        body = page.content.gsub(/<[^>]*>/, '').strip[0..200] rescue ""
 
         json << {
           url: page.url,
-          title: title,
+          title: title.strip,
           body: body
         }
       end
 
+      # Write search-index.json in _site
       file_path = File.join(site.dest, 'search-index.json')
+      FileUtils.mkdir_p(File.dirname(file_path))
       File.write(file_path, JSON.pretty_generate(json))
     end
   end
